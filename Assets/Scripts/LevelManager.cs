a4a1a4a171cf2083c47a52feb0e1a8fd47bb6971
Marcio Silva <marcioadsworks@gmail.com>
2018-01-29 17:20:56 -0300

    projeto iniciado

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string level){
+
+		SceneManager.LoadScene(level);
+	}
+
+	public  void QuitRequest(){
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel (){
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+
+	public void BrickDestroyed(){
+		
+			LoadNextLevel ();
+		}
+	}
+
